9a8250b9e5b2962536c93a6b6fe582d82e704852
Raj kothari <rkmeansrk39@gmail.com>
2020-07-26 02:43:09 +0530

    initial commit

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,10 +1,21 @@
 const router = require("express").Router();
 const CreateStudent = require("./createstudent");
+const CreateTask = require("./createtask");
+const FetchTask = require("./fetchtask");
+const SubmitTask = require("./submittask");
+const multer = require("multer");
+var storage = multer.memoryStorage();
+var upload = multer({ storage: storage });
+
+// const upload = multer({ dest: "uploads/" });
 
 router.post("/createstudent", CreateStudent.createStudent);
 router.post(
   "/createinstructor",
   require("./createinstructor").createInstructor
 );
+router.get("/getAllTask", FetchTask.fetchAllTask);
+router.post("/createtask", upload.single("image"), CreateTask.createTask);
+router.post("/submitTask", upload.single("image"), SubmitTask.submitTask);
 
 module.exports = router;

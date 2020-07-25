8170fc5dfa6bd55650374572b66f853190d653d3
Raj kothari <rkmeansrk39@gmail.com>
2020-07-26 02:42:56 +0530

    initial commit

--- routes/v1/createtask.js
+++ routes/v1/createtask.js
@@ -1,7 +1,42 @@
 const Task = require("../../database/task");
+const req = require("postman-request");
+const util = require("util");
+const reqPost = util.promisify(req.post);
+require("dotenv").config({ path: `${__dirname}/config.env` });
 
 exports.createTask = async (req, res) => {
   try {
+    const encoded = req.file.buffer.toString("base64");
+    let options = {
+      key: "38fa6122d68d5de7b0076375dcd96148",
+      image: encoded,
+    };
+    let imgUpload = await reqPost({
+      url: "https://api.imgbb.com/1/upload",
+      form: options,
+      method: "POST",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+    });
+    imgUpload = JSON.parse(imgUpload.body);
+    if (imgUpload.status !== 200) {
+      throw Error("not able to upload image");
+    }
+    console.log(imgUpload.data.image.url);
+    const insertResult = await Task.insert(
+      req.body.instructorId,
+      imgUpload.data.image.url,
+      req.body.explanation,
+      req.body.level
+    );
+    if (insertResult.status == true) {
+      res.status(200).json({
+        req_result: true,
+      });
+      return;
+    }
+    throw Error("not able to put the task");
   } catch (err) {
     console.log(err);
     res.status(400).json({

ecf2115ddbb28c06d2d2c6685cda97abbe52b049
Raj kothari <rkmeansrk39@gmail.com>
2020-07-25 10:42:59 +0530

    initial commit

---
+++ database/instructor.js
@@ -0,0 +1,5 @@
+const pg = require("../pg/pg");
+
+class Instructor {}
+
+module.exports = Instructor;
---
+++ database/student.js
@@ -0,0 +1,5 @@
+const pg = require("../pg/pg");
+
+class Student {}
+
+module.exports = Student;

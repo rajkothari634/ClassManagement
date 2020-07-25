6b97af21982da95ebf6d960c2f236ea3b366b4e9
Raj kothari <rkmeansrk39@gmail.com>
2020-07-25 16:56:46 +0530

    initial commit

--- database/instructor.js
+++ database/instructor.js
@@ -1,5 +1,58 @@
 const pg = require("../pg/pg");
 
-class Instructor {}
+class Instructor {
+  static async insert(instructor_id, name, password, level) {
+    const query = `INSERT INTO INSTRUCTOR (
+            instructor_seq_no,
+            instructor_id,
+            name,
+            password,
+            level
+        )
+        VALUES(
+            nextval('INSTRUCTOR_SEQ_NO'),$1,$2,$3,$4
+        );`;
+    try {
+      console.log("database student started");
+      const result = await pg.query(query, [
+        instructor_id,
+        name,
+        password,
+        level,
+      ]);
+      console.log(result + "fff");
+      if (result.rowCount === 1) {
+        return {
+          status: true,
+          body: result,
+        };
+      } else {
+        throw Error(result);
+      }
+    } catch (err) {
+      return {
+        status: false,
+        err_code: err,
+      };
+    }
+  }
+
+  static async findInstructorById(id) {
+    const query = `SELECT * FROM INSTRUCTOR WHERE instructor_id = $1`;
+    try {
+      const result = await pg.query(query, [id]);
+      if (result.rowCount === 1) {
+        return {
+          status: true,
+        };
+      }
+      throw Error("not found");
+    } catch (err) {
+      return {
+        status: false,
+      };
+    }
+  }
+}
 
 module.exports = Instructor;

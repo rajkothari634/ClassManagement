b45edc8d2a81f7cca0ca21c4250fb301eec4ab6b
Raj kothari <rkmeansrk39@gmail.com>
2020-07-25 16:57:20 +0530

    initial commit

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -1,3 +1,10 @@
 const router = require("express").Router();
+const CreateStudent = require("./createstudent");
+
+router.post("/createstudent", CreateStudent.createStudent);
+router.post(
+  "/createinstructor",
+  require("./createinstructor").createInstructor
+);
 
 module.exports = router;

d8888136a901852f1aca19c0f267e6806f2c4bdb
Raj kothari <rkmeansrk39@gmail.com>
2020-07-25 16:59:03 +0530

    initial commit

--- database/student.js
+++ database/student.js
@@ -1,5 +1,43 @@
 const pg = require("../pg/pg");
 
-class Student {}
+class Student {
+  static async insert(student_id, name, password, level, instructor) {
+    const query = `INSERT INTO STUDENT (
+            student_seq_no,
+            student_id,
+            name,
+            password,
+            level,
+            instructor_id
+        )
+        VALUES(
+            nextval('STUDENT_SEQ_NO'),$1,$2,$3,$4,$5
+        );`;
+    try {
+      console.log("database student started");
+      const result = await pg.query(query, [
+        student_id,
+        name,
+        password,
+        level,
+        instructor,
+      ]);
+      console.log(result + "fff");
+      if (result.rowCount === 1) {
+        return {
+          status: true,
+          body: result,
+        };
+      } else {
+        throw Error(result);
+      }
+    } catch (err) {
+      return {
+        status: false,
+        err_code: err,
+      };
+    }
+  }
+}
 
 module.exports = Student;

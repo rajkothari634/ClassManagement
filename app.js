6f40cecfeb9395748b015c043c5b1b6892f0b602
Raj kothari <rkmeansrk39@gmail.com>
2020-07-26 02:40:36 +0530

    initial commit

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
 const app = require("express")();
-const parser = require("body-parser");
-app.use(parser.json());
+const bodyParser = require("body-parser");
+// const CreateTask = require("./routes/v1/createtask");
+// const multer = require("multer");
+// const upload = multer({ dest: "uploads/" });
+
+app.use(bodyParser.json());
+
 app.use("/v1", require("./routes/v1/index"));
 
 module.exports = app;

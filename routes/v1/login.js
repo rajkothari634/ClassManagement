8e54f0e517a0b39f2a4f22db1553ef6630f70d7b
Raj kothari <rkmeansrk39@gmail.com>
2020-07-26 19:25:57 +0530

    update

---
+++ routes/v1/login.js
@@ -0,0 +1,42 @@
+const Student = require("../../database/student");
+const Instructor = require("../../database/instructor");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+
+exports.login = async (req, res) => {
+  try {
+    var result;
+    req.body.password = await bcrypt.hash(req.body.password, 12);
+    if (req.body.type == "instructor") {
+      result = await Instructor.checkUser(req.body.id, req.body.password);
+    } else {
+      result = await Student.checkUser(req.body.id, req.body.password);
+    }
+    if (result.status == true) {
+      const token = jwt.sign(
+        {
+          id: req.body.id,
+        },
+        "secret-key-needed-for-jwt-token",
+        {
+          expiresIn: "90d",
+        }
+      );
+      res.status(200).json({
+        result: true,
+        token: token,
+      });
+    } else {
+      throw Error("user not found");
+    }
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({
+      req_result: false,
+      err_info: {
+        err_code: 400,
+        err_txt: err,
+      },
+    });
+  }
+};

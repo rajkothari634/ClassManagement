9ee580c041e85ae627ad067caa13f4c71c73fedb
Raj kothari <rkmeansrk39@gmail.com>
2020-07-26 19:29:49 +0530

    update

--- routes/v1/index.js
+++ routes/v1/index.js
@@ -6,6 +6,8 @@ const StuTaskPerformance = require("./stutaskperformance");
 const SubmitTask = require("./submittask");
 const PutGrade = require("./putgrade");
 const GetGrade = require("./getgrades");
+const Login = require("./login");
+const ProtectRoute = require("./protectroute");
 const multer = require("multer");
 var storage = multer.memoryStorage();
 var upload = multer({ storage: storage });
@@ -17,11 +19,26 @@ router.post(
   "/createinstructor",
   require("./createinstructor").createInstructor
 );
-router.get("/getAllTask", FetchTask.fetchAllTask);
-router.post("/createtask", upload.single("image"), CreateTask.createTask);
-router.post("/submitTask", upload.single("image"), SubmitTask.submitTask);
-router.post("/stuTaskPerformance", StuTaskPerformance.StudentTaskPerformance);
-router.post("/putGrade", PutGrade.putGrade);
-router.get("/getGrade", GetGrade.getGrades);
+router.get("/getAllTask", ProtectRoute.protect, FetchTask.fetchAllTask);
+router.post(
+  "/createtask",
+  ProtectRoute.protect,
+  upload.single("image"),
+  CreateTask.createTask
+);
+router.post(
+  "/submitTask",
+  ProtectRoute.protect,
+  upload.single("image"),
+  SubmitTask.submitTask
+);
+router.post(
+  "/stuTaskPerformance",
+  ProtectRoute.protect,
+  StuTaskPerformance.StudentTaskPerformance
+);
+router.post("/putGrade", ProtectRoute.protect, PutGrade.putGrade);
+router.get("/getGrade", ProtectRoute.protect, GetGrade.getGrades);
+router.post("/login", Login.login);
 
 module.exports = router;

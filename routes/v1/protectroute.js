106c2636d8f512fb45b77d7e9198fd681b7fc3c4
Raj kothari <rkmeansrk39@gmail.com>
2020-07-26 19:27:00 +0530

    update

---
+++ routes/v1/protectroute.js
@@ -0,0 +1,32 @@
+const { JsonWebTokenError } = require("jsonwebtoken");
+
+const jwt = require("jsonwebtoken");
+const { promisify } = require("util");
+
+exports.protect = async (req, res, next) => {
+  try {
+    console.log("inside protect");
+    let token;
+    if (req.headers.authorization) {
+      token = req.headers.authorization;
+    } else {
+      throw Error("Login Require");
+    }
+    //verify token
+    const decoded = await promisify(jwt.verify)(
+      token,
+      "secret-key-needed-for-jwt-token"
+    );
+    console.log(decoded);
+    req.jwtId = decoded.id;
+    next();
+  } catch (err) {
+    res.status(400).json({
+      req_result: "F",
+      err_info: {
+        err_code: 400,
+        err_txt: "Login Required",
+      },
+    });
+  }
+};
